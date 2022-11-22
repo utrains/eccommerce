885b3956cb29cb40f16b0663b0799ac18e1332e4
noeltatah <noel.tatah@utrains.org>
2022-11-22 22:22:58 +0100

    added dockerignor start-server.sh nginx,default and modified the settings

--- demo/settings.py
+++ demo/settings.py
@@ -66,22 +66,22 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media_root')
 
-#DATABASES = {
- #   "default": {
-  #      "ENGINE": "django.db.backends.sqlite3",
-   #     "NAME": os.path.join(BASE_DIR, 'db.sqlite3')
-    #}
-#}
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'website',
-        'USER': 'webuser',
-        'PASSWORD': 'utrainsdb',
-        'HOST': 'db',
-        'PORT': '',
+   "default": {
+       "ENGINE": "django.db.backends.sqlite3",
+       "NAME": os.path.join(BASE_DIR, 'db.sqlite3')
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'website',
+#         'USER': 'webuser',
+#         'PASSWORD': 'utrainsdb',
+#         'HOST': 'db',
+#         'PORT': '',
+#     }
+# }
 
 
 if ENVIRONMENT == 'production':
